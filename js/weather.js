fae66e6e03955bc690a7ae2ce7bbe33af3d31920
kemiedon <kemiedon@gmail.com>
2022-12-22 11:22:18 +0800

    1.2

--- js/weather.js
+++ js/weather.js
@@ -5,7 +5,7 @@ $(function() {
         url: "https://json2jsonp.com/?url=https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-063",
         dataType: 'json',
         headers: {
-            'Access-Control-Allow-Origin': 'http://clientdomain.dev'
+            'Access-Control-Allow-Origin': '*'
         },
         data: { Authorization: "CWB-A13D4A9A-CCDC-492F-B370-A3E7289AE3C7", elementName: "T" },
         success: res => {
