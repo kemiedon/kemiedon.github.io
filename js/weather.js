5ff706d88be5e85e63108d6e349e3db472e442e1
kemiedon <kemiedon@gmail.com>
2022-12-22 14:56:08 +0800

    2.1
    帶入資料到html

--- js/weather.js
+++ js/weather.js
@@ -5,7 +5,10 @@ $(function() {
         url: cors + "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-063?Authorization=CWB-A13D4A9A-CCDC-492F-B370-A3E7289AE3C7&elementName=T",
         dataType: 'json',
         success: res => {
-            console.log(res.records.localtions[0].time)
+            console.log(res.records.localtions[0].weatherElement[0].time)
+            $('#city_name').html(res.records.locations[0].locationsName);
+            $('#district').html(res.records.locations[0].localtion[0].locationName);
+            $('#tempture').html(res.records.locations[0].localtion[0].weatherElement[0].time[0].elementValue.value + "&#176;");
         }
     });
 });
