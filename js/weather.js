669435375aba7e5424a0f5f17119540ab1594e81
kemiedon <kemiedon@gmail.com>
2022-12-22 11:19:33 +0800

    1.1
    update weather.js

--- js/weather.js
+++ js/weather.js
@@ -2,14 +2,14 @@ $(function() {
 
     $.ajax({
         type: "GET",
-        url: "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-063",
+        url: "https://json2jsonp.com/?url=https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-063",
         dataType: 'json',
         headers: {
-            "Authorization": "CWB-A13D4A9A-CCDC-492F-B370-A3E7289AE3C7"
+            'Access-Control-Allow-Origin': 'http://clientdomain.dev'
         },
-        data: { elementName: "T" },
-        success: function() {
-            alert('Thanks for your comment!');
+        data: { Authorization: "CWB-A13D4A9A-CCDC-492F-B370-A3E7289AE3C7", elementName: "T" },
+        success: res => {
+            console.log(res)
         }
     });
 });
