dd90d1c17c2a5d8ad1be5b8ed9e73c7b1e8098c0
kemiedon <kemiedon@gmail.com>
2022-12-22 12:17:24 +0800

    1.6
    change header rule

--- js/weather.js
+++ js/weather.js
@@ -5,6 +5,8 @@ $(function() {
         url: "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-063?Authorization=CWB-A13D4A9A-CCDC-492F-B370-A3E7289AE3C7&elementName=T",
         dataType: 'json',
         headers: {
+            'Access-Control-Allow-Methods': 'GET, POST',
+            'Access-Control-Allow-Credentials': true,
             'Access-Control-Allow-Origin': '*'
         },
         success: res => {
