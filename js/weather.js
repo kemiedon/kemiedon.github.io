6af307e41e7fb17dd3b1cfa6e8401837dbf7b693
kemiedon <kemiedon@gmail.com>
2022-12-26 20:53:23 +0800

    4.4

--- js/weather.js
+++ js/weather.js
@@ -1,36 +1,33 @@
-const app = {
-    data() {
-        return {
-            city: '',
-            district: '',
-            num: 30,
-            tempture: '',
-            main_icon: '',
-            week: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-            week_tempture: {},
-            icons: ['https://i.imgur.com/Shrg84B.png']
-        }
-    },
-    mounted() {
-        const cors = 'https://cors-anywhere.herokuapp.com/';
-        axios
-            .get(cors + 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-063?Authorization=CWB-A13D4A9A-CCDC-492F-B370-A3E7289AE3C7&locationName=%E4%B8%AD%E6%AD%A3%E5%8D%80&elementName=T')
-            .then(function(res) {
-                console.log(res.data.records);
-                this.city = res.data.records.locations[0].locationsName;
-                this.district = res.data.records.locations[0].location[0].locationName;
-                this.tempture = res.data.records.locations[0].location[0].weatherElement[0].time[0].elementValue[0].value + "&#176;";
-                this.main_icon = (parseInt(this.tempture) > 18) ? 'https://i.imgur.com/Shrg84B.png' : 'https://i.imgur.com/BeWfUuG.png';
-                $('#city_name').html(this.city);
-                $('#district').html(this.district);
-                $('#tempture').html(this.tempture);
-                this.week_tempture = res.data.records.locations[0].location[0].weatherElement[0].time;
+$(function() {
+    const cors = 'https://cors-anywhere.herokuapp.com/';
+    $.ajax({
+        url: "URL",
+        type: "GET",
+        dataType: "json",
+        success: function(res) {
+            const week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+            const html = `<div class="d-flex flex-column block"><small class="text-muted mb-0">`;
+            const html2 = `</small><div class="text-center"><img class="symbol-img" src="`;
+            const html3 = `"></div><h6><strong>`;
+            const html4 = `&#176;</strong></h6>`;
+            let week_html = '';
+            $('#city_name').html(res.records.locations[0].locationsName);
+            $('#district').html(res.records.locations[0].location[0].locationName);
+            $('#tempture').html(res.records.locations[0].location[0].weatherElement[0].time[0].elementValue[0].value + "&#176;");
 
-                console.log(this.week_tempture);
-            })
-            .catch(function(error) { // 请求失败处理
-                console.log(error);
-            });
-    }
-}
-Vue.createApp(app).mount('#app');
+            for (var i = 1; i < 6; i += 2) {
+                let j = 0;
+                let degree = res.records.locations[0].location[0].weatherElement[0].time[i].elementValue[0].value;
+                icon = (degree > 18) ? "https://i.imgur.com/Shrg84B.png" : "https://i.imgur.com/BeWfUuG.png";
+                week_html += html1 + week[j] + html2 + icon
+                week_html += html3 + degree + html4;
+                console.log(week_html);
+                j++;
+            }
+        },
+
+        error: function() {
+            alert("ERROR!!!");
+        }
+    });
+});
