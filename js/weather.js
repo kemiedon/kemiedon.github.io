47ac1ade500f6dce97c6005dfe50a48fc089c0bb
kemiedon <kemiedon@gmail.com>
2022-12-26 15:19:49 +0800

    2.8
    delete ajax code

--- js/weather.js
+++ js/weather.js
@@ -1,27 +1,3 @@
-$(function() {
-    const cors = 'https://cors-anywhere.herokuapp.com/';
-    $.ajax({
-        type: "GET",
-        url: cors + "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-063?Authorization=CWB-A13D4A9A-CCDC-492F-B370-A3E7289AE3C7&locationName=%E4%B8%AD%E6%AD%A3%E5%8D%80&elementName=T",
-        dataType: 'json',
-        success: res => {
-            console.log(res.records)
-            $('#city_name').html(res.records.locations[0].locationsName);
-            $('#district').html(res.records.locations[0].location[0].locationName);
-            $('#tempture').html(res.records.locations[0].location[0].weatherElement[0].time[0].elementValue[0].value + "&#176;");
-            const week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-            let html1 = `<div class="d-flex flex-column block first-block">
-            <small class="text-muted mb-0">`
-            week[i] +
-                let html2 = `</small><div class="text-center"><img class="symbol-img" src="`
-            let html3 = `"></div><h6><strong>` + `</strong></h6></div>`
-
-        }
-    });
-
-
-
-});
 const app = {
     data() {
         return {
