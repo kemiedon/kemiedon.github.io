b0a0ef47647ae2b0a681a816c7ba9c727bc581b7
kemiedon <kemiedon@gmail.com>
2022-12-26 19:26:06 +0800

    4.1

--- js/weather.js
+++ js/weather.js
@@ -13,6 +13,8 @@ const app = {
     },
     mounted() {
         const cors = 'https://cors-anywhere.herokuapp.com/';
+        this.week_tempture[0] = null;
+        this.icons[0] = null;
         axios
             .get(cors + 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-063?Authorization=CWB-A13D4A9A-CCDC-492F-B370-A3E7289AE3C7&locationName=%E4%B8%AD%E6%AD%A3%E5%8D%80&elementName=T')
             .then(function(res) {
@@ -21,7 +23,7 @@ const app = {
                 this.district = res.data.records.locations[0].location[0].locationName;
                 this.tempture = res.data.records.locations[0].location[0].weatherElement[0].time[0].elementValue[0].value + "&#176;";
                 main_icon = (parseInt(this.tempture) > 18) ? 'https://i.imgur.com/Shrg84B.png' : 'https://i.imgur.com/BeWfUuG.png';
-                for (let i = 0; i < res.data.records.locations[0].location[0].weatherElement[0].time.length; i += 2) {
+                for (let i = 1; i < res.data.records.locations[0].location[0].weatherElement[0].time.length; i += 2) {
                     let j = 0
                     var degree = res.data.records.locations[0].location[0].weatherElement[0].time[i].elementValue[0].value;
                     console.log(degree);
