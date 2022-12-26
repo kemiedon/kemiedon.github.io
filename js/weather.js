78f9958edc8e5ec4c71bcb5aca5ba106cc7bc67d
kemiedon <kemiedon@gmail.com>
2022-12-26 16:09:41 +0800

    3.3

--- js/weather.js
+++ js/weather.js
@@ -14,7 +14,7 @@ const app = {
         axios
             .get(cors + 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-063?Authorization=CWB-A13D4A9A-CCDC-492F-B370-A3E7289AE3C7&locationName=%E4%B8%AD%E6%AD%A3%E5%8D%80&elementName=T')
             .then(function(res) {
-                console.log(res.records);
+                console.log(res);
                 this.city = res.records.locations[0].locationsName;
                 this.district = res.records.locations[0].location[0].locationName;
                 this.tempture = res.records.locations[0].location[0].weatherElement[0].time[0].elementValue[0].value + "&#176;";
