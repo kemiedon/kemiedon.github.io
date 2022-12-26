25d88bf7e0e3995215a48a6c814db33e388083cc
kemiedon <kemiedon@gmail.com>
2022-12-26 20:14:57 +0800

    Revert "2.6"
    This reverts commit 25e5ccb07a041d7c4a0fa9d594f88d524de9fe4d.

--- js/memo.js
+++ js/memo.js
@@ -2,7 +2,7 @@ $(function() {
 
     $.ajax({
         type: "GET",
-        url: "https://8f8bf4c5-f0f3-4ea7-8bd5-b7f96ada0d95.mock.pstmn.io/getMemos",
+        url: cors + "https://8f8bf4c5-f0f3-4ea7-8bd5-b7f96ada0d95.mock.pstmn.io/getMemos",
         dataType: 'json',
         success: res => {
             console.log(res)
